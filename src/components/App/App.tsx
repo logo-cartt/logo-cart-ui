82deef5c1afdaf0a7b3fcf755685480015cb8318
Azimkhan93 <aabdulsatarov@gmail.com>
2025-01-07 14:24:09 +0500

    [LC-22] rearrange product folders

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,9 +2,9 @@ import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "../../theme";
 import { GlobalStyleWrapper } from "../GlobalStyleWrapper";
+import { ProductDetailPage } from "../../modules/product/page/ProductDetailPage";
 // import { LoginPage } from "../../modules/auth/page/LoginPage";
 // import { SignUpPage } from "../../modules/auth/page/SignUpPage";
-import { ProductDetailPage } from "../../modules/main/page/ProductDetailPage";
 // import { CartPage } from "../../modules/cart/page/CartPage";
 // import { MainPage } from "../../modules/main/page/MainPage";
 
